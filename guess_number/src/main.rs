0d211913b85f9d60eb0eac607beb5ab3f88ae16e
zhangzhibang <295854713c@gmail.com>
2022-09-07 23:43:38 +0800

    feat(猜数游戏): 实现循环猜数

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,24 +1,35 @@
 use rand::Rng;
-use std::{io, cmp::Ordering};
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Hello, world!");
 
     let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
     println!("secret: {}", secret_number);
 
-    let mut guess: String = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("error: 读取用户输入失败");
+    loop {
+        println!("请输入你要猜测的数字：");
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("error: 读取用户输入失败");
 
-    println!("guess: {}", guess);
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("请输入数字！！");
+                continue;
+            }
+        };
 
-    let guess: i32 = guess.trim().parse().expect("error: 请输入数字");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => print!("太小"),
-        Ordering::Greater => print!("太大"),
-        Ordering::Equal => print!("你猜对了"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小"),
+            Ordering::Greater => println!("太大"),
+            Ordering::Equal => {
+                print!("你猜对了");
+                break;
+            }
+        }
     }
 }
