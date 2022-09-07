55a6f42bfaff75873554e8c11a6bc00185278a49
zhangzhibang <295854713c@gmail.com>
2022-09-07 23:27:02 +0800

    feat(猜数游戏): 编写大小比较代码

--- guess_number/src/main.rs
+++ guess_number/src/main.rs
@@ -1,10 +1,24 @@
-use std::io;
+use rand::Rng;
+use std::{io, cmp::Ordering};
 
 fn main() {
     println!("Hello, world!");
 
-    let mut guess: String = String::new();
-    io::stdin().read_line(&mut guess).expect("error");
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    println!("secret: {}", secret_number);
 
-    println!("{}", guess);
+    let mut guess: String = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("error: 读取用户输入失败");
+
+    println!("guess: {}", guess);
+
+    let guess: i32 = guess.trim().parse().expect("error: 请输入数字");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => print!("太小"),
+        Ordering::Greater => print!("太大"),
+        Ordering::Equal => print!("你猜对了"),
+    }
 }
