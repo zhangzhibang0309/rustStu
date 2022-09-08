88625d94213dd2a9bef43d6b352ede57995ef1f6
zhangzhibang <295854713c@gmail.com>
2022-09-08 15:37:16 +0800

    feat(通用基础知识): 简单数据类型

--- basic_knowledge/src/main.rs
+++ basic_knowledge/src/main.rs
@@ -1,11 +1,48 @@
 fn main() {
+    // 1.变量相关知识
     // 变量
-    let x: i32 = 5;
-    println!("x的值是 {}", x);
-    let x: &str = "六";
-    println!("x的值是 {}", x);
-
+    // let x: i32 = 5;
+    // println!("x的值是 {}", x);
+    // let x: &str = "六";
+    // println!("x的值是 {}", x);
     // 常量
-    const MONEY: u32 = 100_000;
-    println!("{}", MONEY);
+    // const MONEY: u32 = 100_000;
+    // println!("{}", MONEY);
+
+    // 2.类型相关知识
+    // 标量
+
+    // 整数类型
+    // let a: i32 = 90_000; // 十进制
+    // let b: i32 = 0x100; // 十六进制
+    // let c: i32 = 0o11; // 八进制
+    // let d: i32 = 0b11; // 二进制
+    // 整数溢出问题
+    // let f: u8 = 256; 八位无符号0-255，256溢出
+    // println!("{}", a);
+    // println!("{}", b);
+    // println!("{}", c);
+    // println!("{}", d);
+    // 浮点数类型
+    // let f1: f32 = 1.11;
+    // let f2: f64 = 10.232323444;
+    // 布尔类型
+    // let b1: bool = true;
+    // let b2: bool = false;
+    // 字符类型
+    // let ch1: char = '1';
+    // let cat: char = '🐱';
+    // let hhh: char = '😄';
+    // println!("{}{}{}", ch1, cat, hhh);
+
+    // 复合类型
+    // tuple
+    // let tup = ("hhhh", 22);
+    // let (channel, age) = tup;
+    // let chan = tup.1;
+    // println!("{} {} {}", channel, age, chan);
+    // array
+    let arr: [i32; 3] = [1, 2, 3];
+    let arr2: [i32; 3] = [1; 3];
+    println!("{} {}", arr[0], arr2[2]);
 }
